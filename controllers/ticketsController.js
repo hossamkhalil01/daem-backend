c29adbf9b70f16c454a98c43bb6101723c5dc950
Hossam Khalil <hossamkhalil001@gmail.com>
2021-06-16 21:46:14 +0200

    Feature(pagination): added pagination config for get all tickets endpoint

--- controllers/ticketsController.js
+++ controllers/ticketsController.js
@@ -1,5 +1,34 @@
+const Ticket = require("../models/ticket");
+const { extractPaginationInfo } = require("../utils/pagination");
+const {
+  statusCodes,
+  sendError,
+  sendResponse,
+  errorMessages,
+} = require("../utils/responses");
+
+
+
 const getTickets = (req, res) => {
-  res.send("it works");
+  // process the query params
+  const [{ limit, page }, filter] = extractPaginationInfo(req.query);
+
+  // the pagination options
+  const options = {
+    sort: { _id: -1 },
+    populate: ["patient", "doctor"],
+    page,
+    limit,
+  };
+
+  try {
+    // get the tickets
+    const tickets = await Ticket.paginate(filter, options);
+    // build the resulting object
+    return sendResponse(res, tickets, statusCodes.success.ok);
+  } catch (error) {
+    return sendError(res, error.message, statusCodes.error.invalidData);
+  }
 };
 
 const getTicket = (req, res) => {
