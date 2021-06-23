12b05116e187ee345bd37d96dddb450d5ef981e5
Hossam Khalil <hossamkhalil001@gmail.com>
2021-06-23 14:46:13 +0200

    Feature(admin-controls): completed usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -30,26 +30,73 @@ const getUsers = async (req, res) => {
   }
 };
 
-const getUser = (req, res) => {
-  res.send("getUser works");
+const getUser = async (req, res) => {
+
+  const id = req.params.id;
+
+  try {
+    const user = await User.findOne({ _id: id }).select("-password");
+    // user not found
+    if (!user)
+      return sendError(res, errorMessages.notFound, statusCodes.error.notFound);
+
+    return sendResponse(res, user, statusCodes.success.ok);
+  } catch (error) {
+    return sendError(
+      res,
+      errorMessages.notFound,
+      statusCodes.error.invalidData
+    );
+  }
 };
 
-const createUser = (req, res) => {
-  res.send("createUser works");
+const updateUser = async (req, res, id, updates) => {
+
+  try {
+    const updatedUser = await User.findOneAndUpdate({ _id: id }, updates, {
+      new: true,
+      runValidators: true,
+    });
+
+    // user not found
+    if (!updatedUser)
+      return sendError(res, errorMessages.notFound, statusCodes.error.notFound);
+
+    // updated
+    return sendResponse(res, updatedUser, statusCodes.success.ok);
+
+  } catch (error) {
+    // invalid params
+    return sendError(res, error.message, statusCodes.error.invalidData);
+  }
+
+}
+
+const updateUserRole = (req, res) => {
+
+  const id = req.params.id;
+  const updates = { role: req.body.role };
+
+  return updateUser(req, res, id, updates)
 };
 
-const updateUser = (req, res) => {
-  res.send("updateUser works");
-};
+const updateCurrentUser = (req, res) => {
 
-const deleteUser = (req, res) => {
-  res.send("deleteUser works");
-};
+  const id = req.user._id;
+  const updates = req.body;
+
+  if (req.file) {
+    avatar = req.file.destination + req.file.filename;
+    updates.avatar = avatar;
+  }
+
+  return updateUser(req, res, id, updates)
+}
+
 
 module.exports = {
   getUsers,
   getUser,
-  createUser,
-  updateUser,
-  deleteUser,
+  updateUserRole,
+  updateCurrentUser
 };
--- routes/users.js
+++ routes/users.js
@@ -6,42 +6,37 @@ const passport = require("passport");
 // init router
 const Router = express.Router();
 
+// add auth mdidleware
+Router.use(passport.authenticate("jwt", { session: false }));
+
 /** 
 GET 
 Route: / 
 Results: All Users
 **/
-Router.get("/",
-  [passport.authenticate("jwt", { session: false }),
-  ],
-  usersController.getUsers);
+Router.get("/", isModerator, usersController.getUsers);
 
 /** 
 GET 
-Route: /id 
+Route: /:id 
 Results: get User
 **/
 Router.get("/:id", usersController.getUser);
 
-/** 
-POST 
-Route: / 
-Results: create User
-**/
-Router.post("/", usersController.createUser);
 
 /** 
-PUT
-Route: /id 
-Results: update User
+Patch
+Route: /:id 
+Results: update User role (moderator only)
 **/
-Router.get("/:id", usersController.updateUser);
+Router.patch("/:id", isModerator, usersController.updateUserRole);
 
 /** 
-DELETE 
+Patch
 Route: / 
-Results: delete Users
+Results: update user (current user's data)
 **/
-Router.get("/:id", usersController.deleteUser);
+Router.patch("/", usersController.updateCurrentUser);
+
 
 module.exports = Router;
