a63db2a339f56c6f2131eea165ea0496c085dc8f
Aya <ayahamedabdulaziz@gmail.com>
2021-06-27 18:39:51 +0200

    feature(article-controllers): upload,update and delete article image

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -6,10 +6,11 @@ const {
   errorMessages,
 } = require("../utils/responses");
 const { extractPaginationInfo } = require("../utils/pagination");
+const uploadObject = require("../middlewares/ticketImagesUpload");
+const { deleteFile } = require("../utils/fileSystem");
 
 const getArticles = async (req, res) => {
   const [{ limit, page }, filter] = extractPaginationInfo(req.query);
-  console.log(filter);
   const options = {
     sort: { createdAt: -1 },
     populate: [{ path: "author", select: "firstname lastname avatar role" }],
@@ -24,11 +25,11 @@ const getArticles = async (req, res) => {
   }
 };
 
-
 const getArticle = async (req, res) => {
   try {
-    const article = await Article.findOne({ _id: req.params.id }).populate("author");
-    console.log(article);
+    const article = await Article.findOne({ _id: req.params.id }).populate(
+      "author"
+    );
     if (!article)
       return sendError(res, errorMessages.notFound, statusCodes.error.notFound);
     return sendResponse(res, article, statusCodes.success.ok);
@@ -38,30 +39,58 @@ const getArticle = async (req, res) => {
 };
 
 const createArticle = async (req, res) => {
-  try {
-    const article = await Article.create(req.body);
-    return sendResponse(res, article, statusCodes.success.created);
-  } catch (error) {
-    return sendError(res, error.message, statusCodes.error.invalidData);
-  }
+  const upload = uploadObject.single("image");
+  upload(req, res, async function (err) {
+    if (err) {
+      return sendError(
+        res,
+        errorMessages.invalidMediaType,
+        statusCodes.error.invalidMediaType
+      );
+    }
+    let newArticle = req.body;
+    if (typeof req.body !== "object") newArticle = JSON.parse(req.body);
+    newArticle.image = req.file.destination + req.file.filename;
+    try {
+      const article = await Article.create({ ...newArticle });
+      return sendResponse(
+        res,
+        createTokenResponse(article),
+        statusCodes.success.created
+      );
+    } catch (error) {
+      newArticle.image ? deleteFile(newArticle.image) : "";
+      return sendError(res, error.message, statusCodes.error.invalidData);
+    }
+  });
 };
 
 const updateArticle = async (req, res) => {
-  try {
-    const updatedArticle = await Article.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body,
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-    if (!updatedArticle)
-      return sendError(res, errorMessages.notFound, statusCodes.error.notFound);
-    return sendResponse(res, updatedArticle, statusCodes.success.ok);
-  } catch (error) {
+  const upload = uploadObj.single("image");
+  const id = req.params.id;
+  upload(req, res, async (err) => {
+    if (err) {
+      return sendError(
+        res,
+        errorMessages.invalidMediaType,
+        statusCodes.error.invalidMediaType
+      );
+    }
+
+    let updates = req.body;
+    if (typeof req.body !== "object") updates = JSON.parse(req.body);
+    if (req.file) {
+      updates.image = req.file.destination + req.file.filename;
+    }
+    const updatedArticle = await Ticket.findOneAndUpdate({ _id: id }, updates, {
+      new: true,
+      runValidators: true,
+    });
+    if (updatedArticle)
+      return sendResponse(res, updatedArticle, statusCodes.success.ok);
+    updates.image ? deleteFile(updates.image) : "";
     return sendError(res, error.message, statusCodes.error.invalidData);
-  }
+  });
 };
 
 const deleteArticle = async (req, res) => {
@@ -71,6 +100,7 @@ const deleteArticle = async (req, res) => {
     });
     if (!deletedArticle)
       return sendError(res, errorMessages.notFound, statusCodes.error.notFound);
+    deleteFile(deletedArticle.image);
     return sendResponse(res, deletedArticle, statusCodes.success.ok);
   } catch (error) {
     return sendError(res, error.message, statusCodes.error.invalidData);
